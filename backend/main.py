0b9edfa96b3aaefe82efb23083429e50f62338fd
Abhinav Jha <jhaabhinavkumar1@gmail.com>
2024-06-23 12:12:07 +0530

    first commit

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from core.config import settings
+from apis.base_router import api_router
+
+
+def include_router(app):
+    app.include_router(api_router)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    include_router(app)
+    return app
+
+
+app = start_application()
+
+
+@app.get("/")
+def hello():
+    return {"msg": "Hello FastAPI 🚀"}
