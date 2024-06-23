9281713e3899e7ec0cba2313db0576e55e314328
Abhinav Jha <jhaabhinavkumar1@gmail.com>
2024-06-23 19:17:58 +0530

    get all active blogs

--- backend/apis/v1/route_blog.py
+++ backend/apis/v1/route_blog.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, status, Depends, HTTPException
+from typing import List
 from sqlalchemy.orm import Session
 from db.session import get_db
 from schemas.blog import CreateBlog, ShowBlog
-from db.repository.blog import create_new_blog, retrieve_blog
+from db.repository.blog import create_new_blog, retrieve_blog, list_all_active_blogs
 
 router = APIRouter()
 
@@ -22,3 +23,9 @@ def get_blog(id: int, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
         )
     return blog
+
+
+@router.get("", response_model=List[ShowBlog])
+def get_all_active_blogs(db: Session = Depends(get_db)):
+    blogs = list_all_active_blogs(db=db)
+    return blogs
